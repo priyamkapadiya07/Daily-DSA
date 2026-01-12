7d2c74618feef6cdc6beb3d7dc3d7835428d6758
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-12 12:33:07 +0530

    Arrays Problem Day-2 Min and Max in Array

---
+++ Arrays/Min and Max in Array.py
@@ -0,0 +1,10 @@
+arr=input("Enter elements of array separated by space: ").split()
+min=arr[0]
+max=arr[0]
+for i in arr:
+    if i<min:
+        min=i
+    if i>max:
+        max=i
+print("Minimum element is:",min)
+print("Maximum element is:",max)
