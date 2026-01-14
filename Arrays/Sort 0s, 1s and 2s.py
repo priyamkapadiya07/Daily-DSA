80edfec7c1abf489953466c309696abc023f749a
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-14 15:17:39 +0530

    Array problem Program to sort an array consisting of only 0s, 1s and 2s

---
+++ Arrays/Sort 0s, 1s and 2s.py
@@ -0,0 +1,11 @@
+# Program to sort an array consisting of only 0s, 1s and 2s
+
+arr = list(map(int, input("Enter elements of array (0s, 1s and 2s) separated by space: ").split()))
+d={}
+for i in arr:
+    d[i]=d.get(i,0)+1
+    
+result = []
+for i,j in d.items():
+    result.extend([i]*j)
+print("Sorted array of 0s, 1s and 2s is:", sorted(result))
