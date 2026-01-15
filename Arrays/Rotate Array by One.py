0ead3a7fcda5f8626aad488b01dfc27efc14daa2
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-15 17:22:18 +0530

    Write a program to cyclically rotate an array by one.

--- Arrays/Rotate Array by One.py
+++ Arrays/Rotate Array by One.py
@@ -1,6 +1,6 @@
 # Write a program to cyclically rotate an array by one.
 
-arr = [1, 2, 3, 4, 5]
+arr = list(map(int, input("Enter elements of array separated by space: ").split()))
 n=int(input("Enter no. of rotation you want: "))
 while n>0:
     arr.insert(0,arr.pop())
