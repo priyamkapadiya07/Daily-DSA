1263a8bd788dd33fb42cb7fc9b0d86b7e03aff89
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-17 16:32:43 +0530

    Find duplicate in an array of N+1 Integers

--- Arrays/Array Duplicates.py
+++ Arrays/Array Duplicates.py
@@ -1,8 +1,23 @@
 # Find duplicate in an array of N+1 Integers
 
+# Method - 1 (Using Dictionary)
 arr = list(map(int, input("Enter elements of array separated by space: ").split()))
 d={}
 for i in arr:
     d[i]=d.get(i,0)+1
 duplicate=list(k for k,v in d.items() if v>1)
 print(duplicate)
+
+
+# Method - 2 (Using Set)
+arr = list(map(int, input("Enter elements of array separated by space: ").split()))
+s=set()
+duplicate=[]
+for i in arr:
+    if i in s:
+        duplicate.append(i)
+    else:
+        s.add(i)
+print(duplicate)
+
+
