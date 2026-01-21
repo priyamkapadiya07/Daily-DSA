d61ebee21d7c636afc49dd153c16fa43507b12a0
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-21 12:34:09 +0530

    Count Inversions

--- Arrays/Count Inversions.py
+++ Arrays/Count Inversions.py
@@ -1,11 +1,12 @@
 # Count Inversions
 
+def count_inversions(arr):
+    c = 0
+    for i in range(len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                c += 1
+    return c
+
 arr = list(map(int, input("Enter elements of array separated by space: ").split()))
-
-c = 0
-
-for i in range(len(arr)-2):
-    for j in range(i+1, len(arr)-1):
-        if arr[i] > arr[j]:
-            c += 1
-print(c)
+print(count_inversions(arr))
