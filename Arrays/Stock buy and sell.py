44b4538dfe8e3f5db348858dd7f9d13e27c98e98
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-22 11:58:01 +0530

    Best time to buy and Sell stock

--- Arrays/Stock buy and sell.py
+++ Arrays/Stock buy and sell.py
@@ -1,10 +1,22 @@
 # Best time to buy and Sell stock
 
+"""
+Time Complexity: O(n)
+Reason: We iterate through the array once from index 1 to n-1
+
+Space Complexity: O(1)
+Reason: We only use constant extra space for the ans variable, 
+        regardless of input size
+"""
+
+
 def stockBuySell(arr):
-    ans=0
-    for i in range(1,len(arr)):
-        if arr[i]>arr[i-1]:
-            ans+=arr[i]-arr[i-1]
+    ans = 0
+    
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            ans += arr[i] - arr[i - 1]
+    
     return ans
 
 arr = list(map(int, input("Enter stock prices separated by space: ").split()))
