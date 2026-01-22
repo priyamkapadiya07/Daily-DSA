6aa947b98c9c40fa4a31d96798adad593084aa36
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-22 11:55:15 +0530

    Best time to buy and Sell stock

--- Arrays/Stock buy and sell.py
+++ Arrays/Stock buy and sell.py
@@ -2,8 +2,11 @@
 
 arr =  [4, 2]
 
-ans=0 
-for i in range(1,len(arr)):
-    if arr[i]>arr[i-1]:
-        ans+=arr[i]-arr[i-1]
-print(ans)
+
+def stockBuySell(arr):
+    ans=0
+    for i in range(1,len(arr)):
+        if arr[i]>arr[i-1]:
+            ans+=arr[i]-arr[i-1]
+    return ans
+print(stockBuySell(arr))
