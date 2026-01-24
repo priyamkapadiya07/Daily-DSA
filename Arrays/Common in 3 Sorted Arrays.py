16cd7c0cbe2f718e904ef4861fc1b19b12c912fc
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-24 18:10:38 +0530

    Find common elements In 3 sorted arrays

--- Arrays/Common in 3 Sorted Arrays.py
+++ Arrays/Common in 3 Sorted Arrays.py
@@ -1,1 +1,26 @@
 # Find common elements In 3 sorted arrays
+
+arr1 = [1, 5, 10, 20, 40, 80]
+arr2 = [6, 7, 20, 80, 100]
+arr3 = [3, 4, 15, 20, 30, 70, 80, 120]
+
+def find_common_elements(arr1, arr2, arr3):
+    i, j, k = 0, 0, 0
+    common_elements = []
+
+    while i < len(arr1) and j < len(arr2) and k < len(arr3):
+        if arr1[i] == arr2[j] == arr3[k]:
+            common_elements.append(arr1[i])
+            i += 1
+            j += 1
+            k += 1
+        elif arr1[i] < arr2[j]:
+            i += 1
+        elif arr2[j] < arr3[k]:
+            j += 1
+        else:
+            k += 1
+
+    return common_elements
+
+print("Common elements in the three sorted arrays are:", find_common_elements(arr1, arr2, arr3))
