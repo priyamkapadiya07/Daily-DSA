2996ce93b27fb80c89b6ecb65b6c54a96c2546a0
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-25 21:42:12 +0530

    Rearrange the array in alternating positive and negative items with O(1) extra space

--- Arrays/Rearrange Array Alternately.py
+++ Arrays/Rearrange Array Alternately.py
@@ -1,10 +1,10 @@
 # Rearrange the array in alternating positive and negative items with O(1) extra space
 
-# arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
-# def rearrange_alternately(arr):
-#     i=0
-#     while i<len(arr):
-#         arr.insert(i,arr.pop())
-#         i+=2
-#     return arr
-# print(rearrange_alternately(sorted(arr)))
+arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+def rearrange_alternately(arr):
+    i=0
+    while i<len(arr):
+        arr.insert(i,arr.pop())
+        i+=2
+    return arr
+print(rearrange_alternately(sorted(arr)))
