2fa7d021994b553af79e9c74cad41424b919211c
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-26 11:17:30 +0530

    Find if there is any subarray with sum equal to 0

--- Arrays/Subarray with 0 sum.py
+++ Arrays/Subarray with 0 sum.py
@@ -1,1 +1,12 @@
 # Find if there is any subarray with sum equal to 0
+
+arr = [1, 2, -1]
+def has_zero_sum_subarray(arr):
+    for i in range(len(arr)):
+        curr_sum = 0
+        for j in range(i, len(arr)):
+            curr_sum += arr[j]
+            if curr_sum == 0:
+                return arr[i:j+1]
+    return "no element"
+print(has_zero_sum_subarray(arr))
