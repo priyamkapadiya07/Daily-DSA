443fe2b4e896c0441c8d3bb3a3f986ede709d9d2
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-27 11:19:53 +0530

    Find factorial of a large number

--- Arrays/Factorials of large numbers.py
+++ Arrays/Factorials of large numbers.py
@@ -1,1 +1,11 @@
 # Find factorial of a large number
+
+n = int(input("Enter a large number to find its factorial: "))
+def factorial_large_number(num):
+    result=1
+    for i in range(1,num+1):
+        result *= i
+    return result
+
+re = list(map(int, str(factorial_large_number(n))))
+print(f"The factorial of {n} is: {re}")
