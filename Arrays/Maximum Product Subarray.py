eaadea07994ff0edbc2fd3b5fd231de6960c64b1
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-28 10:20:30 +0530

    Find maximum product subarray

--- Arrays/Maximum Product Subarray.py
+++ Arrays/Maximum Product Subarray.py
@@ -1,1 +1,10 @@
 # Find maximum product subarray
+
+arr = [-2, 6, -3, -10, 0, 2]
+
+for i in range(len(arr)):
+    pro=1
+    for j in range(i, len(arr)):
+        pro *= arr[j]
+        arr[i]=max(arr[i], pro)
+print("The maximum product subarray is:", max(arr))
