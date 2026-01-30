8697639cc0a94785fafd2c5d7a2ea74376d79db5
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-01-30 15:30:15 +0530

    Given an array of size n and a number k, fin all elements that appear more than n/k times.

--- Arrays/More than n div k Occurrences.py
+++ Arrays/More than n div k Occurrences.py
@@ -1,14 +1,28 @@
-# Given an array of size n and a number k, fin all elements that appear more than " n/k " times.
+"""
+Given an array of size n and a number k, find all elements that appear more than "n/k" times.
+
+Time Complexity: O(n)
+    - First loop iterates through all n elements: O(n)
+    - Second loop iterates through unique elements (at most n): O(n)
+    - Total: O(n)
+
+Space Complexity: O(n)
+    - Dictionary stores at most n unique elements in worst case (all elements are unique)
+    - Uses O(n) space for the dictionary
+"""
 
 arr = list(map(int, input("Enter the elements of the array separated by spaces: ").split()))
 k = int(input("Enter the value of k: "))
 
-d={}
+# Count occurrences of each element
+d = {}
 for i in arr:
-    d[i]=d.get(i,0)+1
-    
-res=0
+    d[i] = d.get(i, 0) + 1
+
+# Count elements that appear more than n/k times
+res = 0
 for key in d:
-    if d[key] > len(arr)//k:
-        res+=1
+    if d[key] > len(arr) // k:
+        res += 1
+
 print(res)
