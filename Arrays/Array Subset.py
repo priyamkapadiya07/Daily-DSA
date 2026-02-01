751c741967ecabf269219d7400d4cb87051d28a2
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-01 17:03:21 +0530

    Find whether an array is a subset of another array

--- Arrays/Array Subset.py
+++ Arrays/Array Subset.py
@@ -2,3 +2,14 @@
 Find whether an array is a subset of another array
 '''
 
+a = [11, 7, 1, 13, 21, 3, 7, 3]
+b = [11, 3, 7, 1, 7]
+
+def isSubset(arr1, arr2):
+    set_a = set(arr1)
+    for i in arr2:
+        if i not in set_a:
+            return False
+    return True
+
+print(isSubset(a, b))
