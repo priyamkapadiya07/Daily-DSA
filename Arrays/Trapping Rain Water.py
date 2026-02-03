9b99bd8e7d4044d44f8b2d85c288bc917ff66c21
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-03 11:15:32 +0530

    Trapping Rain water problem

--- Arrays/Trapping Rain Water.py
+++ Arrays/Trapping Rain Water.py
@@ -9,7 +9,29 @@ for i in range(len(a)-1):
 print(l)
 
 # right max
-for i in range(len(r)-1,0,-1):
-    if r[i-1]<r[i]:
-        r[i-1]=r[i]
+for j in range(len(r)-1,0,-1):
+    if r[j-1]<r[j]:
+        r[j-1]=r[j]
 print(r)
+
+# def trap(arr):
+#     left = 0
+#     right = len(arr) - 1
+#     leftMax = rightMax = 0
+#     water = 0
+
+#     while left < right:
+#         if arr[left] < arr[right]:
+#             leftMax = max(leftMax, arr[left])
+#             water += leftMax - arr[left]
+#             left += 1
+#         else:
+#             rightMax = max(rightMax, arr[right])
+#             water += rightMax - arr[right]
+#             right -= 1
+
+#     return water
+
+# arr = [3,0,1,0,4,0,2]
+# result = trap(arr)
+# print("Total trapped rain water:", result)
