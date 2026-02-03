2601f4abed683f7b1ea2b11543a234e122cbe5b4
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-03 11:09:22 +0530

    Trapping Rain water problem

--- Arrays/Trapping Rain Water.py
+++ Arrays/Trapping Rain Water.py
@@ -1,7 +1,15 @@
 '''Trapping Rain water problem'''
-
 a=[3,0,1,0,4,0,2]
+l=a
+r=a
+# left max
 for i in range(len(a)-1):
-    if a[i]>a[i+1]:
-        a[i+1]=a[i]
-print(a)
+    if l[i]>l[i+1]:
+        l[i+1]=l[i]
+print(l)
+
+# right max
+for i in range(len(r)-1,0,-1):
+    if r[i-1]<r[i]:
+        r[i-1]=r[i]
+print(r)
