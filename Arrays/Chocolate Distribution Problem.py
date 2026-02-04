da4e95b37e79dbb228db0a684d779a271a8cbb77
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-04 10:20:56 +0530

    Chocolate Distribution Problem

--- Arrays/Chocolate Distribution Problem.py
+++ Arrays/Chocolate Distribution Problem.py
@@ -1,2 +1,22 @@
 # Chocolate Distribution Problem
 
+arr = [7, 3, 2, 4, 9, 12, 56]
+m = 3
+def findMinDiff(arr, n, m):
+    if m == 0 or n == 0:
+        return 0
+
+    arr.sort()
+
+    if n < m:
+        return -1
+
+    min_diff = arr[m - 1] - arr[0]
+
+    for i in range(m, n):
+        min_diff = min(min_diff, arr[i] - arr[i - m + 1])
+
+    return min_diff
+n = len(arr)
+result = findMinDiff(arr, n, m)
+print("Minimum difference is", result)
