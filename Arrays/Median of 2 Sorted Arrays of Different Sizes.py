6e0843f07be2b597eaf423f4c20fdcb98f633d8e
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-10 10:25:03 +0530

    Median of 2 Sorted Arrays of Different Sizes

--- Arrays/Median of 2 Sorted Arrays of Different Sizes.py
+++ Arrays/Median of 2 Sorted Arrays of Different Sizes.py
@@ -1,2 +1,18 @@
 # Median of 2 Sorted Arrays of Different Sizes
 
+a = [3, 5, 6, 12, 15]
+b = [3, 4, 6, 10, 10, 12]
+
+def median(arr):
+    arr.sort()
+    n = len(arr)
+
+    if n % 2 == 0:
+        mid1 = n // 2 - 1
+        mid2 = n // 2
+        return (arr[mid1] + arr[mid2]) / 2
+    else:
+        mid = n // 2
+        return arr[mid]
+merged = a + b
+print(median(merged))
