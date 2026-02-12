28a021891ac378b40da7bdf74aa5c89e4dfe1a25
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-12 10:27:33 +0530

    Search an element in a Matrix

--- Matrix/Search in a sorted Matrix..py
+++ Matrix/Search in a sorted Matrix..py
@@ -1,1 +1,17 @@
 # Search an element in a Matrix
+
+mat = [[1, 5, 9, 11], [14, 20, 21, 26], [30, 34, 43, 50]]
+x = 42
+
+def search_matrix(matrix, target):
+    row=0
+    col=len(matrix[0])-1
+    while row<len(matrix) and col>=0:
+        if matrix[row][col]==target:
+            return True
+        elif matrix[row][col]>target:
+            col-=1
+        else:
+            row+=1
+    return False
+print(search_matrix(mat, x))
