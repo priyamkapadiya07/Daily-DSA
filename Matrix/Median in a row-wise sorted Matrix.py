cd51ab491a6bb19e88d5c10442b38ac6311b9fa7
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-13 10:11:50 +0530

    Find median in a row wise sorted matrix

--- Matrix/Median in a row-wise sorted Matrix.py
+++ Matrix/Median in a row-wise sorted Matrix.py
@@ -1,2 +1,14 @@
 # Find median in a row wise sorted matrix
 
+mat = [[1, 3, 5],[2, 6, 9],[3, 6, 9]]
+
+def median(matrix):
+    for i in range(1,len(matrix)):
+        mat[0].extend(matrix[i])
+    mat[0].sort()
+    n = len(mat[0])
+    if n%2==0:
+        return (mat[0][n//2-1]+mat[0][n//2])//2
+    else:
+        return mat[0][n//2]
+        
