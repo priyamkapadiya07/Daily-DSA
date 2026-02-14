047f572084228439c73a6c7c05c28cc24526eca7
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-14 17:19:33 +0530

    Find row with maximum no. of 1's

--- Matrix/Row with max 1s.py
+++ Matrix/Row with max 1s.py
@@ -1,1 +1,13 @@
 # Find row with maximum no. of 1's
+
+arr = [[0,0], [1,1]]
+
+def row_with_max_1s(arr):
+    max_sum=0
+    row = 0
+    for i in range(len(arr)):
+        if max_sum < sum(arr[i]):
+            max_sum = sum(arr[i])
+            row = i
+    return row
+print("Row with maximum 1's is:", row_with_max_1s(arr))
