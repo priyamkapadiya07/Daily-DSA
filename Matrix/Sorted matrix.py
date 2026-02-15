612c3cc7e0f7e8361962d7c30d8bfa4e04f5eded
priyamkapadiya07 <kapadiyapriyam36@gmail.com>
2026-02-15 21:52:34 +0530

    Print elements in sorted order using row-column wise sorted matrix

--- Matrix/Sorted matrix.py
+++ Matrix/Sorted matrix.py
@@ -1,6 +1,15 @@
 # Print elements in sorted order using row-column wise sorted matrix
 
-Mat=[[1,5,3],[2,8,7],[4,6,9]]
+Mat=[[10,20,30,40],[15,25,35,45],[27,29,37,48],[32,33,39,50]]
+N=4
+def sortedMatrix(N,Mat):
+    space=[Mat[i][j] for i in range(N) for j in range(N)]
+    space.sort()
+    k=0
+    for i in range(N):
+        for j in range(N):
+            Mat[i][j]=space[k]
+            k+=1
+    return Mat
 
-def sorted_matrix(mat):
-    
+print("Sorted Matrix is:", sortedMatrix(N, Mat))
